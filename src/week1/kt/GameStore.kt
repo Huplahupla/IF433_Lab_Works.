6a7c639accba9f07b01cbaf79a8bbc1a469943f6
Huplahupla <Albert.ong0505@gmail.com>
2026-09-02 01:04:52 +0700

    week01: (tugas 2) implement discount logic

--- src/week1/kt/GameStore.kt
+++ src/week1/kt/GameStore.kt
@@ -1,6 +1,16 @@
 fun main(){
     val gameTitle: String = "Supra Bapak"
     val price: Int = 300000
+    val discount = calculateDiscount(price)
+    val total = price - discount
 
-    println("Nama Game: $gameTitle ,Harga: $price" )
+    println("Nama Game: $gameTitle ,Harga Asli: Rp $price" )
+
+    println("Status: ${calculateStatus(price)}")
+    println("Harga Akhir: $total" )
+
+
 }
+fun calculateStatus(price :Int ) = if (price > 500000) "discount 20%" else "discount 10%"
+
+fun calculateDiscount (price: Int) = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
