5b0c5eeae6ff3ddec3c12e746248cbe951393428
Huplahupla <Albert.ong0505@gmail.com>
2026-09-05 10:54:31 +0700

    week02: implement user input and pre-validation

---
+++ src/week2/kt/main.kt
@@ -0,0 +1,28 @@
+package week2.kt
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(source = System.`in`)
+
+    println("- - - APLIKASI PBM UMN - - - ")
+
+    println("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    println("Masukkan NIm(Wajib 5 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if (nim.length != 5){
+        println("ERROR: pendaftaran dibatalkan. NIM garus 5 karakter!")
+
+    } else {
+        print("masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = student(name, nim, major)
+        println("Status: pendaftaran selesai.")
+    }
+}
