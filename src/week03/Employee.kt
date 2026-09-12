0cebf093adc4781d16d3f6e9f031eae3e30ec2b8
Huplahupla <Albert.ong0505@gmail.com>
2026-09-12 10:27:55 +0700

    week03: simulate stack overflow error in setter

--- src/week03/Employee.kt
+++ src/week03/Employee.kt
@@ -1,5 +1,9 @@
 package week03
 
-class Employee (val name: String) {
+class employee (val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke : $value")
+            this.salary = value
+        }
 }
---
+++ src/week03/Main.kt
@@ -0,0 +1,7 @@
+import week03.employee
+
+fun main(){
+    val e = employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
