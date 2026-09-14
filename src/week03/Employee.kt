4ae911863cef373a46288755675b70a59a404fb6
Huplahupla <Albert.ong0505@gmail.com>
2026-09-15 02:38:26 +0700

    week03: add private property

--- src/week03/Employee.kt
+++ src/week03/Employee.kt
@@ -1,13 +1,23 @@
 package week03
 
-class employee (val name: String) {
+class employee(val name: String) {
     var salary: Int = 0
         set(value) {
             if (value < 0) {
-                println("ERROR: Gaji tidak boleh negatif! Si-set ke 0. ")
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0. ")
                 field = 0
             } else {
-            field = value
+                field = value
             }
         }
+
+    private var performanceRating: Int = 3
+
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name Meningkat Rating $performanceRating")
+    }
+
+    val tax: Double
+        get() = salary * 0.1
 }
