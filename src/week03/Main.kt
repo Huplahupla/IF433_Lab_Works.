6039cd9ce1eac18b4395296c1c2d759fe80c050d
Huplahupla <Albert.ong0505@gmail.com>
2026-09-15 02:47:00 +0700

    week03: (task 1) implement Weapon testing in main

--- src/week03/Main.kt
+++ src/week03/Main.kt
@@ -1,11 +1,17 @@
 package week03
 
 fun main() {
-    val e = Employee("Budi")
-
+    // 1. Pengujian Employee
+    val e = employee("Budi")
     e.salary = 5000000
     println("Gaji: ${e.salary}")
-
     e.increasePerformance()
     println("Pajak yang harus dibayar: ${e.tax}")
+
+    // 2. Pengujian Weapon (Task 1)
+    println("\n--- Test Task 1: Weapon ---")
+    val pedang = weapon("Excalibur")
+    pedang.damage = -50
+    pedang.damage = 9999
+    println("Tier Pedang: ${pedang.tier}")
 }
